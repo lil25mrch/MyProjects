e60b0d0afd25c704dd54b356ac3eeb9273c960a8
Lilia <lil25mrch@gmail.com>
2020-05-15 21:55:38 +0300

    add semaphoreSlim to limit the number of threads

--- Краулер/PageParser/Helpers/RestHelper.cs
+++ Краулер/PageParser/Helpers/RestHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using PageParser.Helpers.Interfaces;
 using RestSharp;
@@ -6,13 +7,20 @@ using RestSharp;
 namespace PageParser.Helpers {
     public class RestHelper : IWebHelper {
         private static readonly RestClient _restClient = new RestClient();
+        private static readonly SemaphoreSlim _semaphoreSlim = new SemaphoreSlim(0, 10);
 
         public async Task<string> GetContent(string domain) {
-            _restClient.BaseUrl = new Uri(domain);
-            var request = new RestRequest(domain);
-
-            var content = await _restClient.ExecuteGetAsync(request);
-            return content.Content;
+            await _semaphoreSlim.WaitAsync();
+            try {
+                _restClient.BaseUrl = new Uri(domain);
+                var request = new RestRequest(domain);
+                
+                var content = await _restClient.ExecuteGetAsync(request);
+                return content.Content;
+            } finally {
+                _semaphoreSlim.Release();
+            }
         }
     }
 }
+
